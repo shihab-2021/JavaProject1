ed78055078897506ba37ae0e1b35b12bc8c394a8
rakibul58 <rhrahi14@gmail.com>
2022-01-05 13:24:51 +0600

    Rakib's update

--- src/javaproject1/JavaProject1.java
+++ src/javaproject1/JavaProject1.java
@@ -18,6 +18,7 @@ public class JavaProject1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("rakib");
+        System.out.println("This my first push");
     }
     
 }
